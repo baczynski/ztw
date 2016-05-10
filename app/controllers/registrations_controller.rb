a6eed446ece40dd9a2bf6b6fc2986ba8a662db8a
Ivan Nikulin <zip753@gmail.com>
2016-05-10 13:04:30 +0200

    Dodanie edycji danych osobowych

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,26 @@
 class RegistrationsController < Devise::RegistrationsController
+  before_filter :build_address
 
 private
+
+  def build_address
+    if current_player
+      current_player.build_address unless current_player.address
+    end
+  end
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
 
   def sign_up_params
     params.require(:player).permit(:email, :password, :password_confirmation,
                                     :first_name, :surname, :date_of_birth,
+                                    address_attributes: [:city, :zip_code, :street_address])
+  end
+
+  def account_update_params
+    params.require(:player).permit(:email, :first_name, :surname, :date_of_birth,
                                     address_attributes: [:city, :zip_code, :street_address])
   end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -2,12 +2,19 @@ class Player < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable
 
   has_and_belongs_to_many :tournaments
   has_one :address, as: :addressable
 
   accepts_nested_attributes_for :address
 
-  validates :password_confirmation, presence: :true
+  validates :password_confirmation, presence: true, on: :create
+
+  after_initialize :check_address
+
+private
+  def check_address
+    build_address unless address
+  end
 end
